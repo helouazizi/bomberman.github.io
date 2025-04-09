0be6fa28344b0f6ebfbb7a3af9e4c05fa092d4d5
Your Name <you@example.com>
2025-04-09 16:49:36 +0100

    working on the hero clasde

--- logic/hero.js
+++ logic/hero.js
@@ -2,17 +2,19 @@ import { Field } from './field.js'
 export { Hero }
 class Hero extends Field {
     constructor() {
-        super(battleField)
+        super(battleField, width,height)
         this.x=0
         this.y=0
-        this.BattleField=battleField
+        this.battleField=battleField
+        this.width=width
+        this.height=height
     }
     createHero(){
         let hero = document.createElement("div")
         hero.setAttribute("id","hero")
         hero.style.width=`${this.width/15}vw`
         hero.style.height=`${this.height/13}vh`
-        this.BattleField.appendChild(hero)
+        this.battleField.appendChild(hero)
 
     }
     
