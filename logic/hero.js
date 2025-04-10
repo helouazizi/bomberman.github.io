f8f1ecb072155122f31f60b34f644176cf42a6c0
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-04-10 10:09:53 +0100

    p

--- logic/hero.js
+++ logic/hero.js
@@ -1,17 +1,20 @@
-import { Field } from './field.js' 
+import { Field } from './field.js'
 export { Hero }
 class Hero extends Field {
     constructor() {
         super(battleField)
-        this.x=0
-        this.y=0
-        this.battleField=battleField
+        this.x = 0
+        this.y = 0
+        this.battleField = battleField
     }
-    createHero(){
+    createHero() {
         let hero = document.createElement("div")
-        hero.setAttribute("id","hero")
-        hero.style.width=`${parseInt(this.battleField.style.width)/15}vw`
-        hero.style.height=`${parseInt(this.battleField.style.height)/13}vh`
+        hero.setAttribute("id", "hero")
+        hero.style.width = `${parseInt(this.battleField.style.width) / 15}vw`
+        hero.style.height = `${parseInt(this.battleField.style.height) / 13}vh`
         this.battleField.appendChild(hero)
     }
+
+    // Get the position of each element within
+    getPosition = (element) => element.getBoundingClientRect()
 }
