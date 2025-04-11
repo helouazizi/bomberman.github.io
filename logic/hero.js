84894e860e39e2edc22cf3c44b2af72e16999304
hajji ismail <hajji.ismail.851@gmail.com>
2025-04-11 13:58:44 +0100

    working on movemment

--- logic/hero.js
+++ logic/hero.js
@@ -14,109 +14,90 @@ class Hero {
         first.style.position = "relative"
         first.style.overflow = "visible";
         first.appendChild(this.hero)
-       
+
     }
     // create a function  to handle the movement of the hero:
     moveHero() {
-    
+
         let Positions = this.Positions()
-     
-        
+
+
         document.addEventListener("keydown", (e) => {
             console.log(e.key);
-        
+
 
             switch (e.key) {
                 case "ArrowDown":
-                    if(this.moverChecker(Positions,2)){
+                    if (this.moverChecker(Positions, 2)) {
                         this.y++
                     }
-         
+
                     break
                 case "ArrowUp":
-                    if(this.moverChecker(Positions,1)){
+                    if (this.moverChecker(Positions, 1)) {
                         this.y--
 
                     }
-                    
-             
-                       
-                
                     break
 
                 case "ArrowRight":
-                    this.x++
+                    if (this.moverChecker(Positions, 3)) {
+                        this.x++
+                        
+                    }
                     break
                 case "ArrowLeft":
-                    this.x--
+                    if (this.moverChecker(Positions, 4)) {
+                        this.x--
+                    }
+                    break
             }
-            
+
             this.hero.style.transform = `translate( ${this.x}px,${this.y}px)`
         })
     }
-    // get the wall id:
-    getWallId(){
-        let walls=document.querySelectorAll(".wall")
-        let heroPosition = this.getPosition(this.hero)
-        let id = 0
-        walls.forEach(wall => {
-            let wallPosition = this.getPosition(wall)
-          
-        
-            
-            if (wallPosition.top <= heroPosition.top && wallPosition.bottom >= heroPosition.bottom){
-                id = wall.getAttribute("id")
-            }
 
-            
-            
-        });
-        return id
-
-    }
-    // create a checker  function to move the hero in left and right derictions:
-    canMoveRight(id="wall-3"){
-        let wall = document.getElementById(id)
-        let bricks=wall.getElementsByClassName("solid")
-        console.log(bricks);
-        
-
-        
-    }
     // another aproch
-    Positions(){
+    Positions() {
         let battleField = document.getElementById("battleField")
         let walls = battleField.querySelectorAll(".solid")
-   
-        let Positions =[]
-         walls.forEach((wall)=>{
+
+        let Positions = []
+        walls.forEach((wall) => {
             let position = this.getPosition(wall)
             Positions.push(position)
-          
+
         })
         return Positions
-        
-        
-        
-        
+
+
+
+
     }
-    moverChecker(Positions=[],deriction=0){
-        let heroPosition= this.getPosition(this.hero)
+    moverChecker(Positions = [], deriction = 0) {
+        let heroPosition = this.getPosition(this.hero)
         console.log(heroPosition);
         let bool = true
-        
-        Positions.forEach(pos=>{
-           
-            if (deriction ===1 &&( pos.left<= heroPosition.left && pos.right>= heroPosition.right)&& heroPosition.top <= pos.bottom&& heroPosition.bottom >= pos.top){
+
+        Positions.forEach(pos => {
+
+            if (deriction === 1 && (pos.left <= heroPosition.left && pos.right >= heroPosition.right) && heroPosition.top <= pos.bottom && heroPosition.bottom >= pos.top) {
                 bool = false
-            } else if(deriction == 2 && (pos.left<= heroPosition.left && pos.right>= heroPosition.right)&& heroPosition.bottom >= pos.top&& heroPosition.top <= pos.bottom){
+            } else if (deriction == 2 && (pos.left <= heroPosition.left && pos.right >= heroPosition.right) && heroPosition.bottom >= pos.top && heroPosition.top <= pos.bottom) {
                 bool = false
+            } else if (deriction == 3 && (pos.top <= heroPosition.top && pos.bottom >= heroPosition.bottom) && heroPosition.left <= pos.right && heroPosition.right >= pos.left) {
+                bool = false
+
+            } else if (deriction == 4 && (pos.top <= heroPosition.top && pos.bottom >= heroPosition.bottom) && heroPosition.right >= pos.left && heroPosition.left <= pos.right) {
+                bool = false
+                console.log(2);
+                
             }
 
         })
         return bool
-        
-        
+
+
 
 
     }
