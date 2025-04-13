ab9f94a7f9b75c7da99899a85ea98a235ba1acae
Your Name <you@example.com>
2025-04-13 14:37:17 +0100

    moving horesontally

--- logic/bomb.js
+++ logic/bomb.js
@@ -1,23 +1,31 @@
-
-export {Bomb}
+export { Bomb };
 
 class Bomb extends Hero {
-    constructor(hero){
-        this.hero = hero
-    }
+  constructor(hero) {
+    this.hero = hero;
+  }
 
-    // Function to create a bomb (Throw it and run):
-    create() {
-        let container = document.createElement("div")
-        container.setAttribute("id", "bomb")
-        this.hero.appendChild(this.container)
+  // Function to create a bomb (Throw it and run):
+  create() {
+    let container = document.createElement("div");
+    container.setAttribute("id", "bomb");
+    this.hero.appendChild(this.container);
+  }
+  // lets get the boomb locations
+  bringbomblocation() {
+    let heropostions = this.getPosition(this.hero);
+    let elements = document.querySelectorAll(".path");
+    let location 
+    elements.forEach((elment) => {
+        let elmentPositions = this.getPosition(elment)
+        if (elmentPositions.bottom >= heropostions.bottom && elmentPositions.right >= heropostions.right &&  )
+    });
+  }
 
-    }
+  // Create an explosion:
+  explode() {
+    setTimeout(() => {}, 500);
+  }
 
-    // Create an explosion:
-    explode(){
-        setTimeout(()=>{
-
-        }, 500)
-    }
+  getPosition = (element) => element.getBoundingClientRect();
 }
