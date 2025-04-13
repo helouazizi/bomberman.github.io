c7bbb237d769113556db4aa07fd77f71247d3e3d
helouazizi <ouazizi2code@gmail.com>
2025-04-13 14:37:49 +0100

    add

--- logic/bomb.js
+++ logic/bomb.js
@@ -5,7 +5,6 @@ class Bomb extends Hero {
     constructor(hero){
         this.hero = hero
     }
-
     // Function to create a bomb (Throw it and run):
     create() {
         let container = document.createElement("div")
