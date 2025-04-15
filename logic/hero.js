57a984943b10f8ed5782f865f2f82e86d48f4717
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-04-15 11:21:34 +0100

    adjusting throttle

--- logic/hero.js
+++ logic/hero.js
@@ -68,7 +68,7 @@ class Hero {
     });
   }
 
-  // check the horizontal movement
+  // check the horizontal movement:
   canMoveHorizontally(direction = "left") {
     let can = true;
     let elements = this.getHorizontalBricks(direction);
