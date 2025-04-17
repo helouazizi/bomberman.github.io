556a803aadffddc802f2716de84cf9dbeaa79080
helouaziz <ouazizi2code@gmail.com>
2025-04-17 13:42:20 +0100

    seccefully animate the enemus

--- logic/enemies.js
+++ logic/enemies.js
@@ -1,78 +1,68 @@
 import { canMoveHorizontally, canMoveVertically } from "./healpers.js";
 class Enemy {
-  constructor(enemiesCount) {
-    this.enemiesCount = enemiesCount;
+  constructor(id) {
+    this.id = id;
     this.moveright = true;
     this.moveleft = true;
     this.moveup = true;
     this.movedown = true;
+    this.positionX = 0;
+    this.positionY = 0;
+ 
   }
   // create the enemies
-  createEnimies() {
-    this.enemiesCount.forEach((id) => {
-      let Enemy = document.createElement("div");
-      Enemy.classList.add("enemy");
-      let brick = document.getElementById(`${id}`);
-      brick.appendChild(Enemy);
-    });
+  createEnemy() {
+    let Enemy = document.createElement("div");
+    Enemy.classList.add("enemy");
+    Enemy.setAttribute("id", `enemy-${this.id}`);
+    let brick = document.getElementById(`${this.id}`);
+    brick.appendChild(Enemy);
   }
 
   //lets move enemies
-  moveEnimies() {
-    // lets craete enimeis positions
-    let enemiesPositions = [];
-    for (let i = 0; i < this.enemiesCount.size; i++) {
-      let enemyPos = {};
-      enemyPos.x = 0;
-      enemyPos.y = 0;
-      enemiesPositions.push(enemyPos);
-    }
-    let enemies = document.querySelectorAll(".enemy");
-    let firstenemy = enemies[0];
-    firstenemy.style.backgroundColor = "black";
-    console.log(enemies, "enemies");
+  moveEnemy() {
+    let enemy = document.getElementById(`enemy-${this.id}`);
+    enemy.style.backgroundColor = "black";
+    console.log(enemy, "enemies");
 
     // let make the set interval to moove the enenmy
     setInterval(() => {
-  
-      if (canMoveHorizontally(firstenemy, "left") && this.moveleft) {
-        enemiesPositions[0].x--;
-      } else if (canMoveHorizontally(firstenemy, "right") && this.moveright) {
-        enemiesPositions[0].x++;
-      } else if (canMoveVertically(firstenemy, "up") && this.moveup) {
-        enemiesPositions[0].y--;
-      } else if (canMoveVertically(firstenemy, "down") && this.movedown) {
-        enemiesPositions[0].y++;
+      if (canMoveHorizontally(enemy, "left") && this.moveleft) {
+        this.positionX--;
+      } else if (canMoveHorizontally(enemy, "right") && this.moveright) {
+        this.positionX++;
+      } else if (canMoveVertically(enemy, "up") && this.moveup) {
+        this.positionY--;
+      } else if (canMoveVertically(enemy, "down") && this.movedown) {
+        this.positionY++;
       }
 
       // ///////////////////////////////////////////////
-      if (!canMoveHorizontally(firstenemy, "left")) {
+      if (!canMoveHorizontally(enemy, "left")) {
         // block the left
         this.moveleft = false;
         this.moveright = true;
       }
-      if (!canMoveHorizontally(firstenemy, "right")) {
+      if (!canMoveHorizontally(enemy, "right")) {
         // lets block the right
         this.moveright = false;
         this.moveleft = true;
-
       }
-      if (!canMoveVertically(firstenemy, "up")) {
+      if (!canMoveVertically(enemy, "up")) {
         // lets block the top
         this.moveup = false;
         this.movedown = true;
-   
       }
-      if (!canMoveVertically(firstenemy, "down")) {
+      if (!canMoveVertically(enemy, "down")) {
         // lets block the down
         this.movedown = false;
-        this.moveup = true
+        this.moveup = true;
       }
-      firstenemy.style.transform = `translate( ${enemiesPositions[0].x}px,${enemiesPositions[0].y}px)`;
+      enemy.style.transform = `translate( ${this.positionX}px,${this.positionY}px)`;
       console.log("/////////////////////////////////////");
       console.log("top", this.moveup);
       console.log("right", this.moveright);
-      console.log("down",this.movedown);
+      console.log("down", this.movedown);
       console.log("left", this.moveleft);
       // console.log("/////////////////////////////////////");
     }, 20);
