5975f7ecf6b6002a6cc7f16e4ccfbc8e0a1c4ae9
helouaziz <ouazizi2code@gmail.com>
2025-04-27 16:27:48 +0100

    semi final

---
+++ logic/controller.js
@@ -0,0 +1,230 @@
+import { Enemy } from "./enemies.js";
+import { Field } from "./field.js";
+import { Hero } from "./hero.js";
+
+class Control {
+  constructor(stage, lives, score) {
+    this.stage = stage;
+    this.lives = lives;
+    this.score = score;
+    this.gameStatus = "initial";
+    this.controller = null;
+    this.pausebtn = null;
+    this.restartbtn = null;
+    this.resumebtn = null;
+    this.startbtn = null;
+    this.leftTime = 500;
+    this.intervalId = null;
+    this.counter = null;
+    this.attemps = 3;
+    this.isPaused = false;
+    this.enemies = [];
+  }
+
+  createBoard() {
+    const screenWidth = window.innerWidth;
+    // Calculate a responsive unit size based on window width
+    let unitSize = screenWidth * 0.02;
+    unitSize = Math.max(20, Math.min(unitSize, 80));
+    let controller = document.createElement("div");
+    controller.setAttribute("id", "controller");
+    controller.style.width = `${unitSize * 15}px`;
+    let title = document.createElement("h1");
+    let start = document.createElement("button");
+    let restart = document.createElement("button");
+    let resume = document.createElement("button");
+    let pause = document.createElement("button");
+    // Add value to my buttons:
+    start.value = "start";
+    restart.value = "restart";
+    resume.value = "resume";
+    pause.value = "pause";
+    // Add text content to my btns
+    title.textContent = "Bomberman";
+    start.textContent = "start";
+    restart.textContent = "restart";
+    resume.textContent = "resume";
+    pause.textContent = "pause";
+    // set atributes
+    title.setAttribute("class", "title");
+    start.setAttribute("class", "controlBtn");
+    restart.setAttribute("class", "controlBtn");
+    resume.setAttribute("class", "controlBtn");
+    pause.setAttribute("class", "controlBtn");
+    pause.setAttribute("id", "pauseBtn");
+    controller.append(title, start, restart, resume, pause);
+    controller.classList.add("show");
+    document.body.appendChild(controller);
+    // hide the buttons that should:
+    if (this.gameStatus === "initial") {
+      restart.classList.add("hidden");
+      resume.classList.add("hidden");
+      pause.classList.add("hidden");
+      restart.classList.remove("show");
+      resume.classList.remove("show");
+      pause.classList.remove("show");
+    } else {
+      start.classList.remove("show");
+      start.classList.add("hidden");
+      restart.classList.remove("hidden");
+      resume.classList.remove("hidden");
+      pause.classList.remove("hidden");
+      restart.classList.add("show");
+      resume.classList.add("show");
+      pause.classList.add("show");
+      startTimer;
+    }
+    this.controller = controller;
+    this.pausebtn = pause;
+    this.restartbtn = restart;
+    this.startbtn = start;
+    this.resumebtn = resume;
+  }
+
+  startTimer() {
+    if (this.intervalId !== null) return; // Don't start a new timer if one is running
+
+    this.intervalId = setInterval(() => {
+      this.counter.textContent = this.leftTime;
+      this.leftTime--;
+      if (this.leftTime < 0) {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+        this.counter.textContent = "0";
+        let popup = document.createElement("div");
+        popup.setAttribute("id", "popup");
+        popup.classList.add("show");
+        popup.textContent = "Time is up!!!";
+        document.body.appendChild(popup);
+        this.pausebtn.classList.add("dead");
+        this.pausebtn.click();
+        setTimeout(() => {
+          popup.style.display = "none";
+        }, 3000);
+      }
+    }, 1000);
+  }
+
+  pauseTimer() {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+      this.isPaused = true;
+    }
+  }
+
+  resumeTimer() {
+    if (this.isPaused && this.intervalId === null) {
+      this.isPaused = false;
+      this.startTimer();
+    }
+  }
+
+  
+
+  gameController() {
+    if (this.gameStatus === "initial") {
+      console.log("create");
+      this.createBoard();
+      this.gameStatus = "started";
+    }
+    ////////// resizing ///////////////
+    const screenWidth = window.innerWidth;
+    let unitSize = screenWidth * 0.02;
+    unitSize = Math.max(20, Math.min(unitSize, 80));
+    // document.body.innerHTML = "";
+    ////////////// classes ////////////////
+    let field = new Field(unitSize, this.stage);
+    let hero = new Hero(unitSize);
+    ////////////////////////////
+
+    //////////////// events //////////////////
+    let controlBtns = document.querySelectorAll(".controlBtn");
+
+    controlBtns.forEach((btn) => {
+      console.log(btn);
+      btn.addEventListener("click", (e) => {
+        if (e.target.value === "start") {
+          this.pausebtn.classList.remove("hidden");
+          this.pausebtn.classList.add("show");
+          this.startbtn.classList.remove("show");
+          this.startbtn.classList.add("hidden");
+          field.CreateBattleField();
+          this.counter = document.getElementById("timeCounter");
+          hero.createHero();
+          hero.moveHero();
+
+          this.gameController();
+          let randomIds = field.randomEnemies;
+          console.log(randomIds);
+          randomIds.forEach((id) => {
+            let enemy = new Enemy(id, field.width);
+            enemy.createEnemy();
+            enemy.moveEnemy();
+
+            this.enemies.push(enemy);
+          });
+
+          this.startTimer();
+          console.log("interval id: ", this.intervalId);
+        } else if (e.target.value === "restart") {
+          location.reload();
+        } else if (e.target.value === "resume") {
+          this.resumeTimer();
+          this.pausebtn.classList.remove("hidden");
+          this.pausebtn.classList.add("show");
+          this.restartbtn.classList.remove("show");
+          this.resumebtn.classList.remove("show");
+          this.restartbtn.classList.add("hidden");
+          this.resumebtn.classList.add("hidden");
+
+          hero.pause = false;
+          this.enemies.forEach((enemy) => {
+            enemy.resumeAnimation();
+          });
+        } else if (e.target.value === "pause") {
+          this.pauseTimer();
+          this.pausebtn.classList.remove("show");
+          this.pausebtn.classList.add("hidden");
+          this.restartbtn.classList.add("show");
+          this.resumebtn.classList.add("show");
+          this.restartbtn.classList.remove("hidden");
+          this.resumebtn.classList.remove("hidden");
+          if (this.pausebtn.classList.contains("dead")) {
+            this.attemps--;
+            hero.x = 0;
+            hero.y = 0;
+            this.pausebtn.classList.remove("dead");
+            if (this.attemps <= 0) {
+              let popup = document.createElement("div");
+              popup.setAttribute("id", "popup");
+              popup.classList.add("show");
+              popup.textContent = "Game over!!";
+              document.body.appendChild(popup);
+              this.controller.style.display = "none";
+
+              setTimeout(() => {
+                location.reload();
+              }, 2000);
+            }
+          }
+
+          hero.pause = true;
+          console.log("The left time ===>>", this.leftTime);
+
+          this.enemies.forEach((enemy) => {
+            enemy.pauseAnimation();
+          });
+        } else {
+          console.log("Wrong choice!!!");
+        }
+      });
+    });
+
+    /////////////////////  win //////////////
+   
+  }
+}
+
+let controller = new Control(1, 3, 0);
+controller.gameController();
---
+++ logic/healpers.js
@@ -0,0 +1,185 @@
+export function getFourDivs(element, id) {
+  let wall = element.parentElement;
+  let nextWall = wall.nextElementSibling;
+  let prevWall = wall.previousElementSibling;
+
+  let wallNodeList = wall.querySelectorAll(".brick");
+  let nextWallNodeList = nextWall.querySelectorAll(".brick");
+  let prevWallNodeList = prevWall.querySelectorAll(".brick");
+  let index = getDivIndex(wallNodeList, id);
+  //   console.log("index",index);
+  let rightDiv = element.nextElementSibling;
+  let leftDiv = element.previousElementSibling;
+  let topDiv = prevWallNodeList[index];
+  let bottomDiv = nextWallNodeList[index];
+
+  let bombRange = [element, topDiv, rightDiv, bottomDiv, leftDiv];
+  return bombRange;
+}
+// get the bomb index
+function getDivIndex(wall, id) {
+  let index;
+  let element = document.getElementById(id);
+  let parent = element.parentElement;
+  parent.setAttribute("here", "this-one");
+  parent.classList.add("solid")
+
+  // Now you can access each element by index
+  wall.forEach((element, divIndex) => {
+    if (element.hasAttribute("here")) {
+      index = divIndex;
+      element.removeAttribute("here");
+    }
+  });
+  return index;
+}
+
+// Get all the bricks in the horizontal range:
+function getHorizontalBricks(element, direction = "left") {
+  let bricks = document.querySelectorAll(".solid");
+  let position = getPosition(element);
+  let elements = [];
+  bricks.forEach((brick) => {
+    let hinder = getPosition(brick);
+    if (
+      position.top > hinder.top - position.height &&
+      position.bottom < hinder.bottom + position.height
+    ) {
+      if (direction === "left" && position.left > hinder.right) {
+        elements.push(hinder);
+      } else if (direction === "right" && position.right < hinder.left) {
+        elements.push(hinder);
+      }
+    }
+  });
+  return elements;
+}
+function getVerticalBricks(element,direction = "down") {
+  let bricks = document.querySelectorAll(".solid");
+  let position =getPosition(element);
+  let elements = [];
+  bricks.forEach((brick) => {
+    let hinder =getPosition(brick);
+    if (
+      position.right < hinder.right + position.width &&
+      position.left > hinder.left - position.width
+    ) {
+      if (direction === "down" && position.bottom < hinder.top) {
+        elements.push(hinder);
+      } else if (direction === "up" && position.top > hinder.bottom) {
+        elements.push(hinder);
+      }
+    }
+  });
+  return elements;
+}
+
+export function canMoveVertically(element,direction = "down") {
+  let can = true;
+  let elements = getVerticalBricks(element,direction);
+  let position = getPosition(element);
+  elements.forEach((element) => {
+    if (direction === "up") {
+      if (position.top -3 < element.bottom) {
+        can = false;
+      }
+    } else {
+      if (position.bottom + 3 >element.top) {
+        can = false;
+      }
+    }
+  });
+  return can;
+}
+
+export function canMoveHorizontally(element, direction = "left") {
+  let can = true;
+  let elements = getHorizontalBricks(element, direction);
+  let position = getPosition(element);
+  elements.forEach((element) => {
+    if (direction === "right") {
+      if (!(position.right + 3 < element.left)) {
+        can = false;
+      }
+    } else {
+      if (!(position.left -3 > element.right)) {
+        can = false;
+      }
+    }
+  });
+  return can;
+}
+
+export function bringElementAxis(element) {
+  let obj = {};
+  let position = getPosition(element);
+  obj.x = Math.floor(position.x + position.width / 2);
+  obj.y = Math.floor(position.y + position.height / 2);
+  return obj;
+}
+export function isCollistion(target1, targetElement, width) {
+
+  let dead = false;
+  let targetCoordinates = getPosition(targetElement);
+  let target1Coordinates = getPosition(target1);
+  let targetAxis = bringElementAxis(targetElement);
+  let target1Axix = bringElementAxis(target1);
+  if (
+    isCoixial(targetCoordinates, target1Axix, "y") &&
+    targetAxis.y < target1Axix.y &&
+    targetCoordinates.bottom + width > target1Coordinates.top
+  ) {
+    // hero.style.display = "none";
+    dead = true;
+  }
+
+  // top
+  if (
+    isCoixial(targetCoordinates, target1Axix, "y") &&
+    targetAxis.y > target1Axix.y &&
+    targetCoordinates.top - width < target1Coordinates.bottom
+  ) {
+    // hero.style.display = "none";
+    dead = true;
+  }
+  // right
+  if (
+    isCoixial(targetCoordinates, target1Axix, "x") &&
+    targetCoordinates.right + width > target1Coordinates.left &&
+    targetAxis.x < target1Axix.x
+  ) {
+    // hero.style.display = "none";
+    dead = true;
+  }
+
+  //left
+  if (
+    isCoixial(targetCoordinates, target1Axix, "x") &&
+    targetCoordinates.left - width < target1Coordinates.right &&
+    target1Axix.x < targetAxis.x
+  ) {
+    // hero.style.display = "none";
+    dead = true;
+  }
+  return dead;
+}
+ function isCoixial(coordes, axis, direction) {
+  let coixial = false;
+  switch (direction) {
+    case "y":
+      if (axis.x >= coordes.left && axis.x <= coordes.right) {
+        coixial = true;
+      }
+      break;
+
+    case "x":
+      if (axis.y >= coordes.top && axis.y <= coordes.bottom) {
+        coixial = true;
+      }
+      break;
+  }
+
+  return coixial;
+}
+
+export const getPosition = (element) => element.getBoundingClientRect();
